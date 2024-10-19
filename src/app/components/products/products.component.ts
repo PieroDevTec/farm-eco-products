1e678f932b11d9320861ad0ea32e4d4f2b6a9f50
mr.webnet <pierodev5@gmail.com>
2024-10-19 18:07:53 -0500

    la interfaz de productos se ha terminado

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit,AfterViewInit } from '@angular/core';
 import { ProductsService } from "src/app/services/productsService";
 import { NgxSpinnerService } from "ngx-spinner";
 import Swal from 'sweetalert2';
+import { detalleProductoInterface } from 'src/app/utils/interface/detalleProductoInterface';
 
 
 @Component({
@@ -12,6 +13,7 @@ import Swal from 'sweetalert2';
 export class ProductsComponent implements OnInit,AfterViewInit {
 
   listProducts:any[] = [];
+  detalleProducto:detalleProductoInterface = {id:0,title:'',description:'',image:''};
 
   constructor(
     private productsService: ProductsService,
@@ -27,25 +29,41 @@ export class ProductsComponent implements OnInit,AfterViewInit {
       this.spinner.hide();
     }
    });
-
-
-   this.productsService.fnObtenerProducto(30).subscribe({
-    next:(product)=>{
-      console.log(product);
-    },
-    error:(err) => {
-      Swal.fire({
-        title: `<strong>${err.status}</strong>` ,
-        text: err.message,
-        icon: "error"
-      });
-    },
-   })
   }
+
 
   ngAfterViewInit(): void {
   }
 
+  fnDetalleProducto(codproduct:any){
+    this.spinner.show();
+    this.productsService.fnObtenerProducto(codproduct.id).subscribe({
+      next:(product)=>{
+
+        this.detalleProducto = {id:product.id,title:product.title,description:product.description,image:product.images[0]};
+        console.log(this.detalleProducto);
+        this.spinner.hide();
+      },
+      error:(err) => {
+        this.spinner.hide();
+        Swal.fire({
+          title: `<strong>${err.status}</strong>` ,
+          text: err.message,
+          icon: "error"
+        });
+      },
+     })
+  }
+
+  fnComprarProducto(){
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "Compra Exitosa",
+      showConfirmButton: false,
+      timer: 1500
+    });
+  }
 
 
 
---
+++ src/app/utils/interface/detalleProductoInterface.ts
@@ -0,0 +1,7 @@
+
+export interface detalleProductoInterface {
+  id:number,
+  title:string,
+  description:string,
+  image:string
+}
